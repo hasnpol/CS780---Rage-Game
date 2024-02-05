35ba78a7f899cdca540b385f8274d2e05bbc8b1c
Hasan Polat <hep1031@wildcats.unh.edu>
2024-02-05 13:43:29 -0500

    template project

---
+++ android/src/com/ragegame/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.ragegame.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.ragegame.game.RageGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new RageGame(), config);
+	}
+}
