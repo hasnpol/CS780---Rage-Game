18f96bd060d29f57bd6f3e730d5988cad9ec4f91
Martin OSullivan <mvo1005@unh.edu>
2024-03-10 16:13:04 -0400

    title screen and refactored code

--- core/src/com/ragegame/game/RageGame.java
+++ core/src/com/ragegame/game/RageGame.java
@@ -1,6 +1,7 @@
 package com.ragegame.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -25,99 +26,35 @@ import com.ragegame.game.objects.Entity;
 import com.ragegame.game.objects.DynamicEntity.EnemyModel;
 import com.ragegame.game.objects.view.View;
 
+import com.ragegame.game.screens.MainMenu;
 import com.ragegame.game.screens.Map;
 
 import java.util.UUID;
 
-public class RageGame extends ApplicationAdapter {
-	private OrthographicCamera camera;
-	private SpriteBatch batch;
-	public static World world;
-	private Box2DDebugRenderer debugRenderer;
-	private Map gameMap;
+public class RageGame extends Game {
 
-	private ObjectMap<UUID, Entity> gameObjectsToDestroy;
-	private ObjectMap<UUID, Entity> gameObjects;
-	private int screenHeight, screenWidth;
-
-	//private PlayerModel playerModel;
-	//private View playerView;
-	////private EnemyModel enemyModel;
-	//private View enemyView;
-	private PhysicsHandler physicsHandler;
-	private CameraHandler cameraHandler;
-	private BackgroundHandler backgroundHandler;
+	public static final int HEIGHT = 800;
+	public static final int WIDTH = 1000;
+	public SpriteBatch batch;
 
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
-
-		// Init Camera
-		screenWidth = Gdx.graphics.getWidth();
-		screenHeight =  Gdx.graphics.getHeight();
-		camera = new OrthographicCamera(15 , 15 * ((float) screenHeight / screenWidth));
-		cameraHandler = new CameraHandler(camera);
-
-		// Init backgrounds
-		backgroundHandler = new BackgroundHandler();
-
-		// Init the world
-		world = new World(new Vector2(0, -9.8f), true);
-		world.setAutoClearForces(false);
-		world.setGravity(new Vector2(0, -9.8f));
-		debugRenderer = new Box2DDebugRenderer();
-
-		// Init game objects
-		gameObjectsToDestroy = new ObjectMap<>();
-		gameObjects = new ObjectMap<>();
-		this.gameMap = new Map(world, gameObjects, batch, camera);
-
-		// Handle InputProcessor and Contact Listener and Physics Handler
-		InputHandler inputHandler = new InputHandler(gameMap.playerModel);
-		Gdx.input.setInputProcessor(inputHandler);
-		ContactHandler contactHandler = new ContactHandler(world, gameObjects);
-		world.setContactListener(contactHandler);
-		physicsHandler = new PhysicsHandler(world, gameObjects);
+		setScreen(new MainMenu(this));
 
 	}
 
 	@Override
 	public void render() {
-		float dt = Gdx.graphics.getDeltaTime();
 
-		// Clear previous images drawn to the screen
-		ScreenUtils.clear(0, 0, 0, 1);
+		super.render();
 
-		// Handle camera logic so that camera follows player within gameMap bounds
-		cameraHandler.snapToPlayer(gameMap.playerModel.getBody().getPosition(), gameMap.getWidth(), gameMap.getHeight());
-
-		// Draw the background
-		batch.begin();
-		backgroundHandler.render(dt, batch, gameMap.getWidth(), gameMap.getHeight(), gameMap.getPPM());
-		batch.end(); // doing this so that the background is drawn before gameMap don't change this
-
-		// Setup for tiled gameMap to be drawn
-		batch.setProjectionMatrix(camera.combined);
-
-		// Draw the tiled gameMap
-		batch.begin();
-		gameMap.render(dt);
-		batch.end();
-
-		// Physics
-		world.clearForces();
-		gameMap.playerModel.update();
-		physicsHandler.applyForces();
-		physicsHandler.doPhysicsStep(dt);
-		//debugRenderer.render(world, camera.combined);
 
 	}
 
 	@Override
 	public void dispose () {
-		batch.dispose();
-		backgroundHandler.dispose();
-		gameMap.dispose();
+
 	}
 
 }
---
+++ core/src/com/ragegame/game/screens/MainMenu.java
@@ -0,0 +1,91 @@
+package com.ragegame.game.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.ragegame.game.RageGame;
+
+public class MainMenu implements Screen {
+
+    private static final int PLAY_BUTTON_WIDTH = 300;
+    private static final int PLAY_BUTTON_HEIGHT = 120;
+    private static final int PLAY_BUTTON_Y = 230;
+
+    private RageGame game;
+    Texture playButtonActive;
+    Texture playButtonInactive;
+
+    public MainMenu (RageGame game) {
+        this.game = game;
+        playButtonActive = new Texture("menu/play_button_active.png");
+        playButtonInactive = new Texture("menu/play_button_inactive.png");
+
+        final MainMenu mainMenuScreen = this;
+
+        Gdx.input.setInputProcessor(new InputAdapter() {
+
+            @Override
+            public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+
+                //Play game button
+                int x = RageGame.WIDTH / 2 - PLAY_BUTTON_WIDTH / 2;
+                if (Gdx.input.getX() < x + PLAY_BUTTON_WIDTH && Gdx.input.getX() > x && RageGame.HEIGHT - Gdx.input.getY() < PLAY_BUTTON_Y + PLAY_BUTTON_HEIGHT && RageGame.HEIGHT - Gdx.input.getY() > PLAY_BUTTON_Y) {
+                    mainMenuScreen.dispose();
+                    game.setScreen(new GameScreen(game));
+                }
+
+                return super.touchUp(screenX, screenY, pointer, button);
+            }
+
+        });
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.begin();
+
+        int x = RageGame.WIDTH / 2 - PLAY_BUTTON_WIDTH / 2;
+        if (Gdx.input.getX() < x + PLAY_BUTTON_WIDTH && Gdx.input.getX() > x && RageGame.HEIGHT - Gdx.input.getY() < PLAY_BUTTON_Y + PLAY_BUTTON_HEIGHT && RageGame.HEIGHT - Gdx.input.getY() > PLAY_BUTTON_Y) {
+            game.batch.draw(playButtonActive, x, PLAY_BUTTON_Y, PLAY_BUTTON_WIDTH, PLAY_BUTTON_HEIGHT);
+        } else {
+            game.batch.draw(playButtonInactive, x, PLAY_BUTTON_Y, PLAY_BUTTON_WIDTH, PLAY_BUTTON_HEIGHT);
+        }
+
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        Gdx.input.setInputProcessor(null);
+    }
+}
